58b5ab80ddd15c7412c44a4977bcbdd36814f904
Tetiana Tsar <protetsar@gmail.com>
2023-01-26 00:56:06 +0200

    hw-10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
+import { makeMarkupList, makeMarkupItem } from './markUp';
 
+const countryListEl = document.querySelector('.country-list')
+const countryInfoEl = document.querySelector('.country-info')
+const inputSearch = document.getElementById('search-box')
 const DEBOUNCE_DELAY = 300;
+inputSearch.addEventListener('input', debounce(findCountry, DEBOUNCE_DELAY) )
+
+function findCountry(e){
+    e.preventDefault()
+    const inputValue = e.target.value.trim()    
+    fetchCountries(inputValue).then(value =>{ 
+        if(!value.length) throw new Error(error)
+        filterCountry(value)
+    }).catch(onError)
+}
+
+
+
+
+function filterCountry(obj){
+    const n = obj.length
+    if(n > 10){
+        cleanSpace()
+        Notify.info('Too many matches found. Please enter a more specific name')
+    } else if(n === 1){
+        cleanSpace()
+       const countryCard =  makeMarkupItem(obj)
+        countryInfoEl.innerHTML = countryCard
+    } else if(n < 10 && n > 1){
+        cleanSpace()
+     const countryList = makeMarkupList(obj)     
+     countryListEl.innerHTML = countryList
+    }
+}
+
+function cleanSpace(){
+    countryInfoEl.innerHTML = ''
+    countryListEl.innerHTML = ''
+}
+function onError(){
+    cleanSpace()
+    Notify.failure('Oops, there is no country with that name')
+}
---
+++ src/markUp.js
@@ -0,0 +1,27 @@
+function makeMarkupList(arr){
+   return arr.map(({flags, name})=> `<li class="country-item">
+   <img
+   src="${flags.svg}"
+     alt="country flag"
+     class="country-flag"
+   />
+   <p class="country-name">${name}</p>
+ </li>`).join('')
+}
+
+function makeMarkupItem(obj){
+   return obj.map(({name, flags, capital, population, languages})=> `<div class="main-info">
+   <img
+     src=${flags.svg}
+     alt="country flag"
+     class="country-flag"
+   />
+   <h1 class="country-name">${name}</h1>
+ </div>
+ <p class="country-desk"><strong>Capital: </strong>${capital}</p>
+ <p class="country-desk"><strong>Population: </strong>${population}</p>
+ <p class="country-desk"><strong>Languages: </strong>${languages[0].name}</p>`)
+}
+
+export { makeMarkupList, makeMarkupItem }
+
